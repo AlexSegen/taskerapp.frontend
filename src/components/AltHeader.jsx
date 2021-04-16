c3d28ed042e9a77539792fb73b527965ff1f3d40
Alejandro Vivas <alejandrojv17@gmail.com>
2021-04-16 15:35:33 -0400

    Add headlessui

--- src/components/AltHeader.jsx
+++ src/components/AltHeader.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { Fragment, useEffect, useState, useContext } from 'react';
 import { Link, useHistory } from 'react-router-dom'
-import { Menu } from '@headlessui/react';
+import { Menu, Transition } from '@headlessui/react';
 
 import { useAuth } from '../hooks/useAuth';
 import { MenuIcon, BellOutlineIcon } from './Icons';
@@ -48,7 +48,6 @@ const Altheader = () => {
           </div>
           <div className="hidden md:block">
             <div className="flex items-baseline ml-10 space-x-4">
-
               <Link className="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white" to={HOME}>Dashboard</Link>
               <Link className={`nav-item hover:bg-gray-700 hover:text-white ${isActiveLink("/task")}`} to={TASKS}>Tasks</Link>
               <Link className={`nav-item hover:bg-gray-700 hover:text-white ${isActiveLink("/projects")}`} to={PROJECTS}>Projects</Link>
@@ -68,30 +67,47 @@ const Altheader = () => {
             </button>
 
             <Menu className="relative ml-3" as="div">
-              <div>
-                <Menu.Button className="flex items-center max-w-xs text-sm bg-gray-800 rounded-full drowpdown-button focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                  <span className="sr-only">Open user menu</span>
-                  <img className="w-8 h-8 rounded-full drowpdown-button" src={user.avatar} alt={user.first_name}/>
-                </Menu.Button>
-              </div>
+              {({open})=> (
+                <>
+                  <div>
+                    <Menu.Button className="flex items-center max-w-xs text-sm bg-gray-800 rounded-full drowpdown-button focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                      <span className="sr-only">Open user menu</span>
+                      <img className="w-8 h-8 rounded-full drowpdown-button" src={user.avatar} alt={user.first_name}/>
+                    </Menu.Button>
+                  </div>
+                  <Transition 
+                      show={open}
+                      as={Fragment}
+                      enter="transition ease-out duration-100"
+                      enterFrom="transform opacity-0 scale-95"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95"
+                      >
+                  <Menu.Items className={`focus:outline-none block absolute right-0 w-48 py-1 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5`}>
+                    <Menu.Item>
+                      {({active}) => (
+                        <Link className={`block px-4 py-2 text-sm ${active ? 'bg-gray-100':'text-gray-700'}`} role="menuitem" to={PROFILE}>Your Profile</Link>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {({active}) => (
+                        <Link className={`block px-4 py-2 text-sm ${active ? 'bg-gray-100':'text-gray-700'}`} role="menuitem" to={CHANGE_PASSWORD}>Change password</Link>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {({active}) => (
+                      <button onClick={() => Logout()} type="button" className={`block w-full px-4 py-2 text-sm text-left ${active ? 'bg-gray-100':'text-gray-700'}`} role="menuitem">Sign out</button>
+                      )}
+                    </Menu.Item>
+                  </Menu.Items>
+                  </Transition>
+                </>
+              )}
+              
 
-              <Menu.Items className={`focus:outline-none block absolute right-0 w-48 py-1 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5`}>
-                <Menu.Item>
-                  {({active}) => (
-                    <Link className={`block px-4 py-2 text-sm ${active ? 'bg-gray-100':'text-gray-700'}`} role="menuitem" to={PROFILE}>Your Profile</Link>
-                  )}
-                </Menu.Item>
-                <Menu.Item>
-                  {({active}) => (
-                    <Link className={`block px-4 py-2 text-sm ${active ? 'bg-gray-100':'text-gray-700'}`} role="menuitem" to={CHANGE_PASSWORD}>Change password</Link>
-                  )}
-                </Menu.Item>
-                <Menu.Item>
-                  {({active}) => (
-                   <button onClick={() => Logout()} type="button" className={`block w-full px-4 py-2 text-sm text-left ${active ? 'bg-gray-100':'text-gray-700'}`} role="menuitem">Sign out</button>
-                  )}
-                </Menu.Item>
-              </Menu.Items>
+
             </Menu>
           </div>
         </div>
--- src/components/global/UserPicker.jsx
+++ src/components/global/UserPicker.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react'
+import { Menu, Transition } from '@headlessui/react';
 
 import { searchOption } from '../../helpers/utils'
 import { TasksContext } from '../../context/TasksContext';
@@ -8,8 +9,6 @@ const UserPicker = ({onSelect, selected, disabled }) => {
     const { users } = useContext(TasksContext);
     const [user, setUser] = useState(selected);
     const [filtered, setFiltered] = useState([]);
-
-    const [toggle, setToggle] = useState(false);
 
     const handleSelect = u => {
         setUser(u);
@@ -21,16 +20,6 @@ const UserPicker = ({onSelect, selected, disabled }) => {
         setFiltered([...searchOption(users.map(u => ({ ...u, name: `${u.first_name} ${u.last_name}` })), value, "name")])
     }
 
-    const handleToggle = () => {
-        document.addEventListener('click', e => {
-            if(e.target.classList.contains('__userpicker')) {
-                setToggle(!toggle);
-            } else {
-                setToggle(false);
-            }
-        });
-    }
-
     useEffect(() => {
         setUser(selected)
     }, [selected])
@@ -39,57 +28,63 @@ const UserPicker = ({onSelect, selected, disabled }) => {
         setFiltered(users);
     }, [users])
 
-    useEffect(() => {
-        handleToggle();
-
-        return () => {
-            document.removeEventListener('click', () =>{ })
-        }
-    }, [])
-
     return (
-        <div className="relative max-w-md">
-            <button disabled={disabled} type="button"
-            className={`relative rounded flex items-center justify-start px-5 py-2 text-base font-semibold   focus:text-gray-700 focus:outline-none hover:text-gray-700 ${toggle ? 'text-gray-700 bg-blue-100':'text-gray-600'} ${disabled ? '':'hover:bg-blue-100'}`}>
-            {
-                    user ? 
-                        (
-                            <>
-                            <img className="block w-8 mr-3 rounded-full" src={user.avatar} alt=""/> 
-                            <span> {user.first_name} {user.last_name} </span> 
-                            { !disabled && <span onClick={() => handleSelect(null)} role="button" className="relative z-20 ml-2 text-sm font-normal text-gray-500">(remove)</span> }
-                            </>
-                        ) : (
-                            <>
-                            <img className="block w-8 mr-3 rounded-full" src="/avatar.jpg" alt=""/>
-                            <span>Unassigned</span>
-                            </>
-                        )
-                }
-                <div className={`absolute top-0 bottom-0 left-0 right-0 z-10 ${!disabled ? '__userpicker':''}`}></div>
-            </button>
+        <Menu as="div" className="relative max-w-md">
+            {({open}) => (
 
-            {
-                toggle && (
-                    <div className="absolute left-0 z-10 p-4 bg-white rounded-lg shadow-md top-12">
-                        <input onChange={searchItem} className="px-5 py-3 mb-4 text-sm field-control __userpicker" placeholder="Search user..." type="text"/>
-                        <div className="overflow-y-auto" style={{maxHeight: "180px", minWidth: "170px"}}>
+                <>
 
-                            {
-                                filtered.map(u => (
-                                    <div key={u._id} className="flex items-center justify-start mb-2 cursor-pointer" onClick={() => handleSelect(u)}>
-                                        <img className="block w-6 mr-3 rounded-full" src={u.avatar} alt=""/> 
-                                        <span className="flex items-center justify-start text-sm text-gray-600"> {u.first_name} {u.last_name} </span>
-                                    </div>
-                                ))
-                            }
+                    <Menu.Button disabled={disabled} type="button"
+                    className={`relative rounded flex items-center justify-start px-5 py-2 text-base font-semibold text-gray-600 focus:text-gray-700 focus:outline-non focus:bg-blue-100 hover:text-gray-700 ${open ? 'bg-blue-100':''} ${disabled ? '':'hover:bg-blue-100'}`}>
+                        {
+                            user ? 
+                                (
+                                    <>
+                                    <img className="block w-8 mr-3 rounded-full" src={user.avatar} alt=""/> 
+                                    <span> {user.first_name} {user.last_name} </span> 
+                                    { !disabled && <span onClick={() => handleSelect(null)} role="button" className="relative z-20 ml-2 text-sm font-normal text-gray-500">(remove)</span> }
+                                    </>
+                                ) : (
+                                    <>
+                                    <img className="block w-8 mr-3 rounded-full" src="/avatar.jpg" alt=""/>
+                                    <span>Unassigned</span>
+                                    </>
+                                )
+                        }
+                    </Menu.Button>
+                        <Transition 
+                        show={open}
+                        enter="transform transition duration-100 ease-in"
+                        enterFrom="opacity-0 scale-95"
+                        enterTo="opacity-100 scale-100"
+                        leave="transform transition duration-75 ease-out"
+                        leaveFrom="opacity-100 sacale-100"
+                        leaveTo="opacity-0 scale-95"
+                        >
+                        <Menu.Items className="absolute left-0 z-10 p-4 bg-white rounded-lg shadow-md focus:outline-none top-12">
+                            <input onChange={searchItem} className="px-5 py-3 mb-4 text-sm field-control __userpicker" placeholder="Search user..." type="text"/>
+                            <div className="overflow-y-auto" style={{maxHeight: "180px", minWidth: "170px"}}>
 
-                        </div>
-                    </div>
-                )
+                                {
+                                    filtered.map(u => (
+                                        <div key={u._id} className="flex items-center justify-start mb-2 cursor-pointer" onClick={() => handleSelect(u)}>
+                                            <img className="block w-6 mr-3 rounded-full" src={u.avatar} alt=""/> 
+                                            <span className="flex items-center justify-start text-sm text-gray-600"> {u.first_name} {u.last_name} </span>
+                                        </div>
+                                    ))
+                                }
+
+                            </div>
+                        </Menu.Items>
+                    </Transition>
+
+                </>
+            )
+
             }
 
-        </div>
+
+        </Menu>
      );
 }
  
